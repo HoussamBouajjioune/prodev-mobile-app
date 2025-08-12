7eebbf4419eb368ab8f7026a8787ea744db5a953
houssam bouajjioune <houssambouajjioune1998@gmail.com>
2025-08-12 18:37:17 +0100

    ok

--- prodev-mobile-app-0x06/app/_layout.tsx
+++ prodev-mobile-app-0x06/app/_layout.tsx
@@ -3,12 +3,7 @@ import "./global.css"
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{
-      headerShown: false,
-    }}>
-      <Stack.Screen name="/" />
-      <Stack.Screen name="join" />
-      <Stack.Screen name="signin" />
-    </Stack>
+    <Stack screenOptions={{ headerShown: false }} />
+
   );
 }
